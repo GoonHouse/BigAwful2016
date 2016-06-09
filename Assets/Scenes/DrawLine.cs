b0535c1310183d5dc353a9bd293d5ca005a90dc1
TomRintjema <tomrintjema@hotmail.com>
2016-06-09 12:42:28 -0400

    Made a whole new web.

---
+++ Assets/Scenes/DrawLine.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawLine : MonoBehaviour {
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		LineRenderer lineRenderer = GetComponent<LineRenderer> ();
+		Vector3 drawTo = target.transform.position - gameObject.transform.position;
+		lineRenderer.SetPosition (1, drawTo);
+	}
+}
