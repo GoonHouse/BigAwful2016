212f1d91669d839a7b53019f46b25ba792a331af
TomRintjema <tomrintjema@hotmail.com>
2016-07-03 13:24:00 -0400

    Whatever man.

--- Assets/You will die here/Scripts/MoveSucka.cs
+++ Assets/You will die here/Scripts/MoveSucka.cs
@@ -17,12 +17,12 @@ public class MoveSucka : MonoBehaviour {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
-		if(Input.GetButtonDown("Fire2"))
+		if(Input.GetButtonDown("Fire2")) // I think maybe we should make 4 gameobjects pointing North South East West and lerp to each one when the button is pressed or something instead of this
 		{
 			transform.RotateAround (Gramps.transform.position, Vector3.up, turnAmount);
 			Gramps.transform.Rotate (new Vector3(0, turnAmount, 0));
 		}
-
+			
 		Vector3 moveDirection = new Vector3 (-moveHorizontal, 0.0f, -moveVertical);
 
 		transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.World);
