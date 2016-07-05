0015556c458aa5a744e0c694fceae68f8a92f964
EntranceJew <EntranceJew@gmail.com>
2016-07-05 18:44:05 -0400

    Made the random room generation try to weld rooms together.

--- Assets/You will die here/Scripts/RoomGenerator.cs
+++ Assets/You will die here/Scripts/RoomGenerator.cs
@@ -14,7 +14,10 @@ public class RoomGenerator : MonoBehaviour {
     // Use this for initialization
     void Start () {
         SpawnPart("NESW", 0, 0);
-        SpawnPart("N___", 1, 0);
+        SpawnPart("NE_W", 1, 0);  // north =  1,  0
+        SpawnPart("NES_", 0, -1); // east  =  0, -1
+        SpawnPart("_ESW", -1, 0); // south = -1,  0
+        SpawnPart("N_SW", 0, 1);  // west  =  0,  1
     }
 	
 	// Update is called once per frame
@@ -25,8 +28,13 @@ public class RoomGenerator : MonoBehaviour {
             var rx = Random.Range((int)(worldMin.x - 1.0f), (int)(worldMax.x + 2.0f));
             var ry = Random.Range((int)(worldMin.y - 1.0f), (int)(worldMax.y + 2.0f));
             var coord = rx.ToString() + "_" + ry.ToString();
+            
             if (!rooms.ContainsKey(coord)) {
-                SpawnPart("NESW", rx, ry);
+                var part = ProbeSpot(rx, ry);
+                if( part != "no") {
+                    Debug.Log(coord + " -> " + part);
+                    SpawnPart(part, rx, ry);
+                }
             }
 
             /*
@@ -48,11 +56,60 @@ public class RoomGenerator : MonoBehaviour {
         }
     }
 
+    public bool CalcRandomFace() {
+        return Random.value <= 0.75f;
+    }
+
+    public string ProbeSpot(float x, float y) {
+        var str = "";
+
+        var coord = x.ToString() + "_" + y.ToString();
+
+        var coordN = (x + 1).ToString() + "_" + y.ToString();
+        var coordE = x.ToString() + "_" + (y - 1).ToString();
+        var coordS = (x - 1).ToString() + "_" + y.ToString();
+        var coordW = x.ToString() + "_" + (y + 1).ToString();
+
+        if (rooms.ContainsKey(coordN) || rooms.ContainsKey(coordE) || rooms.ContainsKey(coordS) || rooms.ContainsKey(coordW)) {
+            if (!rooms.ContainsKey(coord)) {
+                if (rooms.ContainsKey(coordS) && rooms[coordS].isNorthOpen) {
+                    str += "N";
+                } else {
+                    str += "_";
+                }
+
+                if (rooms.ContainsKey(coordW) && rooms[coordW].isEastOpen) {
+                    str += "E";
+                } else {
+                    str += "_";
+                }
+
+                if (rooms.ContainsKey(coordN) && rooms[coordN].isSouthOpen) {
+                    str += "S";
+                } else {
+                    str += "_";
+                }
+
+                if (rooms.ContainsKey(coordE) && rooms[coordE].isWestOpen) {
+                    str += "W";
+                } else {
+                    str += "_";
+                }
+                return str;
+            } else {
+                return "no";
+            }
+        } else {
+            return "no";
+        }
+    }
+
     public GameObject SpawnPart(string partName, float x, float y) {
         var pos = new Vector3(x * 10.0f, 0, y * 10.0f);
         var part = Resources.Load("Rooms/" + partName);
         var go = Instantiate(part, pos, Quaternion.identity) as GameObject;
-        go.transform.SetParent( GameObject.Find("World").transform );
+        go.transform.SetParent(GameObject.Find("World").transform, true);
+        go.transform.localPosition = pos;
 
         rooms.Add( x.ToString() + "_" + y.ToString(), go.GetComponent<RoomObject>() );
 
