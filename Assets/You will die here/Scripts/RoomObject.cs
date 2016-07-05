afaa6bb20ff77012e3260c27b5f127b8ba84fe32
EntranceJew <EntranceJew@gmail.com>
2016-07-05 14:28:59 -0400

    Added generator scene. Added RoomGenerator on the camera and RoomObject for the room chunks.

---
+++ Assets/You will die here/Scripts/RoomObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomObject : MonoBehaviour {
+
+    public bool isNorthOpen = false;
+    public bool isEastOpen = false;
+    public bool isSouthOpen = false;
+    public bool isWestOpen = false;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
