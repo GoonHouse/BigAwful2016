257e1926a6eea0b1f39e29bde6aef1dfc39b114d
EntranceJew <EntranceJew@gmail.com>
2016-07-05 17:25:11 -0400

    Attempt to generate a room every 3 seconds.

--- Assets/You will die here/Scripts/RoomGenerator.cs
+++ Assets/You will die here/Scripts/RoomGenerator.cs
@@ -8,6 +8,9 @@ public class RoomGenerator : MonoBehaviour {
     public Vector2 worldMin = new Vector2();
     public Vector2 worldMax = new Vector2();
 
+    public float generateFrequency = 3.0f;
+    public float generateTimer = 0.0f;
+
     // Use this for initialization
     void Start () {
         SpawnPart("NESW", 0, 0);
@@ -16,10 +19,36 @@ public class RoomGenerator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        generateTimer -= Time.deltaTime;
+        if( generateTimer <= 0.0f) {
+            generateTimer += generateFrequency;
+            var rx = Random.Range((int)(worldMin.x - 1.0f), (int)(worldMax.x + 2.0f));
+            var ry = Random.Range((int)(worldMin.y - 1.0f), (int)(worldMax.y + 2.0f));
+            var coord = rx.ToString() + "_" + ry.ToString();
+            if (!rooms.ContainsKey(coord)) {
+                SpawnPart("NESW", rx, ry);
+            }
 
-    public GameObject SpawnPart(string partName, int x, int y) {
+            /*
+            var spawned = false;
+            for ( float x = worldMin.x - 1.0f; x <= worldMax.x; x++) {
+                for (float y = worldMin.y - 1.0f; y <= worldMax.y; y++) {
+                    var coord = x.ToString() + "_" + y.ToString();
+                    if ( ! rooms.ContainsKey( coord ) ){
+                        SpawnPart("NESW", x, y);
+                        spawned = true;
+                        break;
+                    }
+                }
+                if( spawned ) {
+                    break;
+                }
+            }
+            */
+        }
+    }
+
+    public GameObject SpawnPart(string partName, float x, float y) {
         var pos = new Vector3(x * 10.0f, 0, y * 10.0f);
         var part = Resources.Load("Rooms/" + partName);
         var go = Instantiate(part, pos, Quaternion.identity) as GameObject;
