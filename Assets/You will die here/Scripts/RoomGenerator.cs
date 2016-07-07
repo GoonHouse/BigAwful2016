891b160fcf5414ef105e933dd5d3752ac1fd2133
EntranceJew <EntranceJew@gmail.com>
2016-07-07 12:29:46 -0400

    Refactored room generation, made orbs more common?

--- Assets/You will die here/Scripts/RoomGenerator.cs
+++ Assets/You will die here/Scripts/RoomGenerator.cs
@@ -19,18 +19,25 @@ public class RoomGenerator : MonoBehaviour {
     public float roomSize = 10.0f;
     public int playerGenerationRadius = 2;
     public float chanceOfWall = 0.75f;
+    public float chanceOfDudRoom = 0.10f;
+
+    private Vector2 D_NORTH = new Vector2(-1,  0);
+    private Vector2 D_EAST  = new Vector2( 0,  1);
+    private Vector2 D_SOUTH = new Vector2( 1,  0);
+    private Vector2 D_WEST  = new Vector2( 0, -1);
 
     // Use this for initialization
     void Start () {
-        SpawnPart("NESW", 0, 0);
-        SpawnPart("NESW", -1, 0);
-        SpawnPart("NESW", 1, 0);
-        SpawnPart("NESW", 0, 1);
-        SpawnPart("NESW", -1, 1);
-        SpawnPart("NESW", 1, 1);
-        SpawnPart("NESW", 0, -1);
-        SpawnPart("NESW", -1, -1);
-        SpawnPart("NESW", 1, -1);
+        var h = new Vector2();
+        SpawnPart("NESW", h);
+        SpawnPart("NESW", h + D_NORTH);
+        SpawnPart("NESW", h + D_NORTH + D_EAST);
+        SpawnPart("NESW", h + D_NORTH + D_WEST);
+        SpawnPart("NESW", h + D_EAST );
+        SpawnPart("NESW", h + D_SOUTH);
+        SpawnPart("NESW", h + D_SOUTH + D_EAST);
+        SpawnPart("NESW", h + D_SOUTH + D_WEST);
+        SpawnPart("NESW", h + D_WEST );
 
         /*
         SpawnPart("NE_W", 1, 0);  // north =  1,  0
@@ -39,76 +46,100 @@ public class RoomGenerator : MonoBehaviour {
         SpawnPart("N_SW", 0, 1);  // west  =  0,  1
         */
 
-        var initGenerateFrequency = generateFrequency;
-        generateFrequency = 0;
         for (int i = 0; i < 500; i++) {
-            Update();
-			/*
-            var rx = Random.Range ((int)(worldMin.x - 1.0f), (int)(worldMax.x + 2.0f));
-			var ry = Random.Range ((int)(worldMin.y - 1.0f), (int)(worldMax.y + 2.0f));
-			var part = ProbeSpot(rx, ry);
-			if( part != "no") {
-				SpawnPart("NESW", rx, ry);
-			}
-            */
+            DoGenerate();
 		}
-        generateFrequency = initGenerateFrequency;
+    }
+    public void DoGenerate() {
+        var r = new Vector2();
+        r.x = Random.Range((int)(worldMin.x - 2.0f), (int)(worldMax.x + 2.0f));
+        r.y = Random.Range((int)(worldMin.y - 2.0f), (int)(worldMax.y + 2.0f));
 
+        MaybeSpawnAt(r);
     }
 	
-	// Update is called once per frame
-	void Update () {
+    public void Generate() {
         generateTimer -= Time.deltaTime;
-        if( generateTimer <= 0.0f) {
+        if (generateTimer <= 0.0f) {
             generateTimer += generateFrequency;
-            var rx = Random.Range((int)(worldMin.x - 1.0f), (int)(worldMax.x + 2.0f));
-            var ry = Random.Range((int)(worldMin.y - 1.0f), (int)(worldMax.y + 2.0f));
-            var coord = rx.ToString() + "_" + ry.ToString();
-            
-            if (!rooms.ContainsKey(coord)) {
-                var part = ProbeSpot(rx, ry);
-                if( part != "no") {
-                    Debug.Log(coord + " -> " + part);
-                    SpawnPart(part, rx, ry);
-                }
-            }
-
-            /*
-            var spawned = false;
-            for ( float x = worldMin.x - 1.0f; x <= worldMax.x; x++) {
-                for (float y = worldMin.y - 1.0f; y <= worldMax.y; y++) {
-                    var coord = x.ToString() + "_" + y.ToString();
-                    if ( ! rooms.ContainsKey( coord ) ){
-                        SpawnPart("NESW", x, y);
-                        spawned = true;
-                        break;
-                    }
-                }
-                if( spawned ) {
-                    break;
-                }
-            }
-            */
-        }
-
-        var pos = GetGridFromWorldPosition(GameObject.Find("GrampsHolder").transform);
-
-        // Spawn an area around the player.
-        for (int i = -playerGenerationRadius; i <= playerGenerationRadius; i++) {
-            for (int j = -playerGenerationRadius; j <= playerGenerationRadius; j++) {
-                MaybeSpawnAt(pos.x + i, pos.y + j);
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.F)) {
-            Debug.Log("PLAYER AT: " + (pos.x).ToString() + "_" + (pos.y).ToString());
+            DoGenerate();
         }
     }
 
-    public void MaybeSpawnAt(float x, float y) {
-        var probe = ProbeSpot(x, y);
-        if( probe != "no" ){
-            SpawnPart(probe, x, y);
+    public void PutFloorNearPlayer(Vector2 loc) {
+        for (int i = -playerGenerationRadius; i <= playerGenerationRadius; i++) {
+            for (int j = -playerGenerationRadius; j <= playerGenerationRadius; j++) {
+                MaybeSpawnAt(loc + new Vector2(i, j));
+            }
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        Generate();
+
+        var pos = GetGridFromWorldPosition(GameObject.Find("GrampsHolder").transform);
+        // Spawn an area around the player.
+        PutFloorNearPlayer(pos);
+
+        if (Input.GetKeyDown(KeyCode.F)) {
+            Debug.Log("PLAYER AT: " + Key(pos));
+        }
+        if (Input.GetKeyDown(KeyCode.G)) {
+            PutFloorNearPlayer(pos);
+        }
+    }
+
+    public void MaybeSpawnAt(Vector2 loc) {
+        var probe = ProbeSpot(loc);
+        if( probe != null ){
+            SpawnPart(probe, loc);
+        }
+    }
+
+    public void MaybeFuckOrbAt(Vector2 loc) {
+        if( !CalcDudRoom() ) {
+            return;
+        }
+        RoomObject roomN, roomE, roomS, roomW;
+        rooms.TryGetValue(Key(loc + D_NORTH), out roomN);
+        rooms.TryGetValue(Key(loc + D_EAST), out roomE);
+        rooms.TryGetValue(Key(loc + D_SOUTH), out roomS);
+        rooms.TryGetValue(Key(loc + D_WEST), out roomW);
+
+        var numNeighbors = 0;
+        var numWalls = 0;
+
+        if( roomN ){
+            numNeighbors++;
+            if( !roomN.isSouthOpen ){
+                numWalls++;
+            }
+        }
+
+        if( roomE ) {
+            numNeighbors++;
+            if( !roomE.isWestOpen ){
+                numWalls++;
+            }
+        }
+
+        if (roomS) {
+            numNeighbors++;
+            if (!roomS.isNorthOpen) {
+                numWalls++;
+            }
+        }
+
+        if (roomW) {
+            numNeighbors++;
+            if (!roomW.isEastOpen) {
+                numWalls++;
+            }
+        }
+
+        if( numWalls == numNeighbors) {
+            SpawnPart("____", loc);
         }
     }
 
@@ -117,7 +148,7 @@ public class RoomGenerator : MonoBehaviour {
     }
 
     public bool CalcDudRoom() {
-        return Random.value <= 0.11f;
+        return Random.value <= chanceOfDudRoom;
     }
 
     public Vector2 GetGridFromWorldPosition(Transform pos) {
@@ -145,105 +176,130 @@ public class RoomGenerator : MonoBehaviour {
         return new Vector2(oX, oY);
     }
 
-    public string ProbeSpot(float x, float y) {
-        var str = "";
-
-        var coord = x.ToString() + "_" + y.ToString();
-
-        var coordN = (x - 1).ToString() + "_" + y.ToString();
-        var coordE = x.ToString() + "_" + (y + 1).ToString();
-        var coordS = (x + 1).ToString() + "_" + y.ToString();
-        var coordW = x.ToString() + "_" + (y - 1).ToString();
-
-        if (rooms.ContainsKey(coordN) || rooms.ContainsKey(coordE) || rooms.ContainsKey(coordS) || rooms.ContainsKey(coordW)) {
-            if (!rooms.ContainsKey(coord)) {
-                //if( CalcDudRoom() ) {
-                //    return "____";
-                //}
-                if (rooms.ContainsKey(coordN) && rooms[coordN].isSouthOpen) {
-                    str += "N";
-                //} else if (rooms.ContainsKey(coordN) && !rooms[coordN].isSouthOpen) {
-                //    return "no";
-                } else if (!rooms.ContainsKey(coordN)) {
-                    if ( CalcRandomFace()) {
-                        str += "N";
-                    } else {
-                        str += "_";
-                    }
-                } else {
-                    str += "_";
-                }
-
-                if (rooms.ContainsKey(coordE) && rooms[coordE].isWestOpen) {
-                    str += "E";
-                //} else if (rooms.ContainsKey(coordE) && !rooms[coordE].isWestOpen) {
-                //    return "no";
-                } else if (!rooms.ContainsKey(coordE)) {
-                    if (CalcRandomFace()) {
-                        str += "E";
-                    } else {
-                        str += "_";
-                    }
-                } else {
-                    str += "_";
-                }
-
-                if (rooms.ContainsKey(coordS) && rooms[coordS].isNorthOpen) {
-                    str += "S";
-                //} else if (rooms.ContainsKey(coordS) && !rooms[coordS].isNorthOpen) {
-                //    return "no";
-                } else if (!rooms.ContainsKey(coordS)) {
-                    if (CalcRandomFace()) {
-                        str += "S";
-                    } else {
-                        str += "_";
-                    }
-                } else {
-                    str += "_";
-                }
-
-                if ( rooms.ContainsKey(coordW) && rooms[coordW].isEastOpen ) {
-                    str += "W";
-                //} else if( rooms.ContainsKey(coordW) && !rooms[coordW].isEastOpen ) {
-                //    return "no";
-                } else if (!rooms.ContainsKey(coordW)) {
-                    if (CalcRandomFace()) {
-                        str += "W";
-                    } else {
-                        str += "_";
-                    }
-                } else {
-                    str += "_";
-                }
-                return str;
-            } else {
-                return "no";
-            }
-        } else {
-            return "____";
-        }
+    public string Key(Vector2 loc) {
+        return loc.x.ToString() + "_" + loc.y.ToString();
     }
 
-    public GameObject SpawnPart(string partName, float x, float y) {
-        var pos = new Vector3(x * roomSize, 0, y * roomSize);
+    public bool CheckHasAnyNeighbors(Vector2 loc) {
+        var coordN = Key(loc + D_NORTH);
+        var coordE = Key(loc + D_EAST);
+        var coordS = Key(loc + D_SOUTH);
+        var coordW = Key(loc + D_WEST);
+
+        return (
+            rooms.ContainsKey(coordN) || rooms.ContainsKey(coordE) || 
+            rooms.ContainsKey(coordS) || rooms.ContainsKey(coordW)
+        );
+    }
+
+    public string ProbeSpot(Vector2 loc) {
+        var str = "";
+
+        var coord = Key(loc);
+
+        RoomObject roomN, roomE, roomS, roomW;
+        rooms.TryGetValue(Key(loc + D_NORTH), out roomN);
+        rooms.TryGetValue(Key(loc + D_EAST ), out roomE);
+        rooms.TryGetValue(Key(loc + D_SOUTH), out roomS);
+        rooms.TryGetValue(Key(loc + D_WEST ), out roomW);
+
+        if (rooms.ContainsKey(coord)) {
+            return null;
+        }
+        if (!CheckHasAnyNeighbors(loc)) {
+            return "____";
+        }
+
+        if (roomN && roomN.isSouthOpen) {
+            str += "N";
+        } else if (roomN == null) {
+            if ( CalcRandomFace() ) {
+                str += "N";
+            } else {
+                str += "_";
+                MaybeFuckOrbAt(loc + D_NORTH + D_NORTH);
+            }
+        } else {
+            str += "_";
+            MaybeFuckOrbAt(loc + D_NORTH + D_NORTH);
+        }
+
+        if (roomE && roomE.isWestOpen) {
+            str += "E";
+        } else if (roomE == null) {
+            if (CalcRandomFace()) {
+                str += "E";
+            } else {
+                str += "_";
+                MaybeFuckOrbAt(loc + D_EAST + D_EAST);
+            }
+        } else {
+            str += "_";
+            MaybeFuckOrbAt(loc + D_EAST + D_EAST);
+        }
+
+        if (roomS && roomS.isNorthOpen) {
+            str += "S";
+        } else if (roomS == null) {
+            if (CalcRandomFace()) {
+                str += "S";
+            } else {
+                str += "_";
+                MaybeFuckOrbAt(loc + D_SOUTH + D_SOUTH);
+            }
+        } else {
+            str += "_";
+            MaybeFuckOrbAt(loc + D_SOUTH + D_SOUTH);
+        }
+
+        if (roomW && roomW.isEastOpen) {
+            str += "W";
+        } else if (roomW == null) {
+            if (CalcRandomFace()) {
+                str += "W";
+            } else {
+                str += "_";
+                MaybeFuckOrbAt(loc + D_WEST + D_WEST);
+            }
+        } else {
+            str += "_";
+            MaybeFuckOrbAt(loc + D_WEST + D_WEST);
+        }
+
+        return str;
+    }
+
+    public GameObject SpawnPart(string partName, Vector2 loc) {
+        var coord = Key(loc);
+
+        if ( rooms.ContainsKey(coord) || partName == null ) {
+            Debug.LogWarning("EITHER " + coord + " ALREADY EXISTS OR " + partName + " ISN'T A PART!");
+            return null;
+        }
+
+        var pos = new Vector3(loc.x * roomSize, 0, loc.y * roomSize);
         var part = Resources.Load("Rooms/" + partName);
         var go = Instantiate(part, pos, Quaternion.identity) as GameObject;
         go.transform.SetParent(GameObject.Find("Rooms").transform, true);
         go.transform.localPosition = pos;
 		go.transform.rotation = GameObject.Find ("Rooms").transform.rotation;
 
-        rooms.Add( x.ToString() + "_" + y.ToString(), go.GetComponent<RoomObject>() );
+        
 
-        if( x < worldMin.x ) {
-            worldMin.x = x;
-        } else if( x > worldMax.x) {
-            worldMax.x = x;
+        go.name = coord + " " + partName;
+
+        rooms.Add( coord, go.GetComponent<RoomObject>() );
+
+        if( loc.x < worldMin.x ) {
+            worldMin.x = loc.x;
+        } else if( loc.x > worldMax.x) {
+            worldMax.x = loc.x;
         }
 
-        if( y < worldMin.y ) {
-            worldMin.y = y;
-        } else if( y > worldMax.y ) {
-            worldMax.y = y;
+        if( loc.y < worldMin.y ) {
+            worldMin.y = loc.y;
+        } else if( loc.y > worldMax.y ) {
+            worldMax.y = loc.y;
         }
 
         return go;
