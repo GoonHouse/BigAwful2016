1a6808ff7e7db97750bb469f2c66e519f9534ee2
TomRintjema <tomrintjema@hotmail.com>
2016-07-07 21:37:40 -0400

    Made a real shit fog controller to fade the fog in. We can update it later so we can fade the fog out when we die and stuff like that.

---
+++ Assets/You will die here/Scripts/FogController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogController : MonoBehaviour {
+	public float closeFogStart = 0;
+	public float endFogStart = 0;
+	public float closeFogTarget = 8;
+	public float endFogTarget = 16;
+	public float fogSpeed = 1;
+
+	private float diff = 0;
+
+	// Use this for initialization
+	void Start () {
+		diff = endFogTarget - closeFogTarget;
+		closeFogStart -= diff;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (endFogStart < endFogTarget) {
+			RenderSettings.fogEndDistance = endFogStart += (fogSpeed * Time.deltaTime);
+		}
+		if (closeFogStart < closeFogTarget) {
+			RenderSettings.fogStartDistance = closeFogStart += (fogSpeed * Time.deltaTime);
+		}
+	}
+}
