f1ac937c3b0f235655500a7e770d80eb0fe7eda8
TomRintjema <tomrintjema@hotmail.com>
2016-07-09 10:47:16 -0400

    Changed how the LookGrandpa.cs works. Deleted NPC until the LookGrandpa.cs is finalized

--- Assets/You will die here/Scripts/LookGrandpa.cs
+++ Assets/You will die here/Scripts/LookGrandpa.cs
@@ -5,28 +5,38 @@ public class LookGrandpa : MonoBehaviour {
 	public float turnSpeed = 1f;
 	public Transform thingToLookAt;
 	public Transform eyeTarget;
+	public Transform grampsHead;
 	public float maxAngle = 0.75f;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+		
 	// Update is called once per frame
+	void Update () {
+		
+	}
+
 	void LateUpdate () {
 		rotateTowards (thingToLookAt.transform.position, eyeTarget.transform.position);
 		//transform.LookAt(thingToLookAt);
 	}
 
+	void OnCollisionEnter(Collision collision){
+		thingToLookAt = collision.gameObject.transform;
+	}
+
 	protected void rotateTowards(Vector3 to, Vector3 eyes) {
 
-		Quaternion lookRotation = Quaternion.LookRotation ((to - transform.position).normalized);
-		Quaternion ahead = Quaternion.LookRotation ((eyes - transform.position).normalized);
+		Quaternion lookRotation = Quaternion.LookRotation ((to - grampsHead.transform.position).normalized);
+		Quaternion ahead = Quaternion.LookRotation ((eyes - grampsHead.transform.position).normalized);
 		float diff = Quaternion.Dot(lookRotation, ahead);
 		diff = Mathf.Abs (diff);
 		if (diff > maxAngle) {
-			transform.rotation = Quaternion.Slerp (transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
+			grampsHead.transform.rotation = Quaternion.Slerp (grampsHead.transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
 		} else {
-			transform.rotation = Quaternion.Slerp (transform.rotation, ahead, Time.deltaTime * turnSpeed);
+			grampsHead.transform.rotation = Quaternion.Slerp (grampsHead.transform.rotation, ahead, Time.deltaTime * turnSpeed);
 		}
 	}
 }
