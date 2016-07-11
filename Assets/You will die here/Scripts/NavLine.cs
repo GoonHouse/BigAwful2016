e6144f896e15bc34f2f7bcf106a02cfd053df09b
TomRintjema <tomrintjema@hotmail.com>
2016-07-11 08:43:00 -0400

    Made black lines point at the black rooms in the void.

---
+++ Assets/You will die here/Scripts/NavLine.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavLine : MonoBehaviour {
+	private GameObject gramps;
+
+	// Use this for initialization
+	void Start () {
+		gramps = GameObject.Find("GrampsHolder");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		LineRenderer lineRenderer = GetComponent<LineRenderer>();
+		lineRenderer.SetPosition (0, transform.position);
+		lineRenderer.SetPosition (1, gramps.transform.position-Vector3.up);
+	}
+}
