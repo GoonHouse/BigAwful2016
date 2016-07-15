823053aa951801890ca3be7c7dc65206b83a2a5b
EntranceJew <EntranceJew@gmail.com>
2016-07-15 13:44:13 -0400

    Put the nav lines above grandpa's head. Because why not?

--- Assets/You will die here/Scripts/NavLine.cs
+++ Assets/You will die here/Scripts/NavLine.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class NavLine : MonoBehaviour {
 	private GameObject gramps;
-
+    private Vector3 grampsHeight = new Vector3(0.0f, 4.5f, 0.0f);
 	// Use this for initialization
 	void Start () {
 		gramps = GameObject.Find("GrampsHolder");
@@ -13,6 +13,6 @@ public class NavLine : MonoBehaviour {
 	void Update () {
 		LineRenderer lineRenderer = GetComponent<LineRenderer>();
 		lineRenderer.SetPosition (0, transform.position);
-		lineRenderer.SetPosition (1, gramps.transform.position-Vector3.up);
+		lineRenderer.SetPosition (1, gramps.transform.position-Vector3.up+grampsHeight);
 	}
 }
