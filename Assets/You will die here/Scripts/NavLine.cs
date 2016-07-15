a680b7e226ce3a51ec6e883a2fbb4df9c64856ab
EntranceJew <EntranceJew@gmail.com>
2016-07-15 14:45:53 -0400

    Knobs. Maybe. I don't know what I did.

--- Assets/You will die here/Scripts/NavLine.cs
+++ Assets/You will die here/Scripts/NavLine.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class NavLine : MonoBehaviour {
 	private GameObject gramps;
     private Vector3 grampsHeight = new Vector3(0.0f, 4.5f, 0.0f);
+    public float grampsLerp = 1.0f;
 	// Use this for initialization
 	void Start () {
 		gramps = GameObject.Find("GrampsHolder");
@@ -12,7 +13,7 @@ public class NavLine : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		LineRenderer lineRenderer = GetComponent<LineRenderer>();
-		lineRenderer.SetPosition (0, transform.position);
-		lineRenderer.SetPosition (1, gramps.transform.position-Vector3.up+grampsHeight);
+		lineRenderer.SetPosition (0, transform.position+grampsHeight);
+		lineRenderer.SetPosition (1, Vector3.Lerp(transform.position+grampsHeight, gramps.transform.position-Vector3.up+grampsHeight, grampsLerp));
 	}
 }
