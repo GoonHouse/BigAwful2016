9e3b74533a4142100ea4a143306c986b4292045c
EntranceJew <EntranceJew@gmail.com>
2016-07-15 12:44:24 -0400

    Fog is now controlled by snapshots. Press "F" to make the scene incomprehensibly pink.

--- Assets/You will die here/Scripts/FogController.cs
+++ Assets/You will die here/Scripts/FogController.cs
@@ -1,36 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
+public class FogSnapshot : System.Object {
+    public FogSnapshot(float startDistance, float endDistance, Color color) {
+        this.startDistance = startDistance;
+        this.endDistance = endDistance;
+        this.color = color;
+    }
+
+    public float startDistance;
+    public float endDistance;
+    public Color color;
+}
+
 public class FogController : MonoBehaviour {
-	public float closeFogStart = 0;
-	public float endFogStart = 0;
-	public float closeFogTarget = 8;
-	public float endFogTarget = 16;
-	public float fogSpeed = 1;
-	public float fogColorSpeed = 1;
-	public Color fogStart;
-	public Color fogEnd;
+    public FogSnapshot thisSnapshot;
+    public FogSnapshot targetSnapshot;
 
-	private float diff = 0;
+    public float timeToStopFog;
+    public float timeToChangeFog = 1.0f;
 
-	//Camera camera;
+    public float timeToStopColor;
+    public float timeToChangeColor = 1.0f;
 
-	// Use this for initialization
-	void Start () {
-		//camera = GetComponent<Camera> ();
-		diff = endFogTarget - closeFogTarget;
-		closeFogStart -= diff;
+    // Use this for initialization
+    void Start () {
+        Change(targetSnapshot, timeToChangeFog, timeToChangeColor);
 	}
+
+    public void Change(FogSnapshot snap, float timeFog, float timeColor) {
+        thisSnapshot = GetFogSnapshot();
+        targetSnapshot = snap;
+
+        timeToChangeFog = timeFog;
+        timeToStopFog = Time.fixedTime + timeToChangeFog;
+
+        timeToChangeColor = timeColor;
+        timeToStopColor = Time.fixedTime + timeToChangeColor;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		if (endFogStart < endFogTarget) {
-			RenderSettings.fogEndDistance = endFogStart += (fogSpeed * Time.deltaTime);
-		}
-		if (closeFogStart < closeFogTarget) {
-			RenderSettings.fogStartDistance = closeFogStart += (fogSpeed * Time.deltaTime);
-		}
-		RenderSettings.fogColor = Color.Lerp (fogStart, fogEnd, fogColorSpeed * Time.time);
-		Camera.main.backgroundColor = Color.Lerp (fogStart, fogEnd, fogColorSpeed * Time.time);
+        if( Time.fixedTime <= timeToStopFog ) {
+            RenderSettings.fogStartDistance = Mathf.Lerp(thisSnapshot.startDistance, targetSnapshot.startDistance, Time.fixedTime / timeToStopFog );
+            RenderSettings.fogEndDistance = Mathf.Lerp(thisSnapshot.endDistance, targetSnapshot.endDistance, Time.fixedTime / timeToStopFog );
+        }
+
+        if( Time.fixedTime <= timeToStopColor) {
+            RenderSettings.fogColor = Color.Lerp(thisSnapshot.color, targetSnapshot.color, Time.fixedTime / timeToStopFog);
+            Camera.main.backgroundColor = RenderSettings.fogColor;
+        }
 	}
+
+    public FogSnapshot GetFogSnapshot() {
+        return new FogSnapshot(RenderSettings.fogStartDistance, RenderSettings.fogEndDistance, RenderSettings.fogColor);
+    }
 }
--- Assets/You will die here/Scripts/Grandpa.cs
+++ Assets/You will die here/Scripts/Grandpa.cs
@@ -12,10 +12,17 @@ public class Grandpa : MonoBehaviour {
     public Vector3 lookDirection = Vector3.zero;
 	private float cameraTargetDirection = 0;
 
+    public bool inControl = true;
+    public Transform moveTarget;
+
     private Animator m_Animator;
     private CharacterController controller;
     private GameObject character;
 	private GameObject cameraHolder;
+
+    public void OnInMoveTarget() {
+
+    }
 
     void Start() {
         m_Animator = GetComponentInChildren<Animator>();
@@ -28,46 +35,57 @@ public class Grandpa : MonoBehaviour {
     }
 
     void Update() {
+        if( inControl ){
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
 
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+            // Are we moving?
+            m_Animator.SetBool("Walking", (moveVertical == 0 && moveHorizontal == 0));
 
-        // Are we moving?
-        m_Animator.SetBool("Walking", (moveVertical == 0 && moveHorizontal == 0));
-
-        if (Input.GetButtonDown("Fire2")) {
-            //Camera.main.transform.RotateAround(transform.position, Vector3.up, cameraTurnAmount);
-            //Camera.main.transform.Rotate(new Vector3(0, cameraTurnAmount, 0));
-			cameraHolder.transform.RotateAround(transform.position, Vector3.up, cameraTurnAmount);
-			cameraTargetDirection += cameraTurnAmount;
-			if(cameraTargetDirection > 359f){
-				cameraTargetDirection = 0f;
-			}
-        }
-
-        // Handle movement.
-        if (controller.isGrounded) {
-            moveDirection = new Vector3(moveHorizontal, 0, moveVertical);
-			moveDirection = Quaternion.AngleAxis (cameraTargetDirection, Vector3.up) * moveDirection;
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= moveSpeed;
-            if (Input.GetButton("Jump")) {
-                moveDirection.y = jumpSpeed;
+            if( Input.GetKeyDown(KeyCode.F)) {
+                var fc = Camera.main.GetComponent<FogController>();
+                var fs = new FogSnapshot(2.0f, 8.0f, new Color(1.0f, 0.0f, 1.0f, 1.0f));
+                fc.Change(fs, 4.0f, 20.0f);
             }
 
-            // Turn if we need to turn.
-            if (moveVertical != 0 || moveHorizontal != 0) {
-                lookDirection = new Vector3(-moveHorizontal, 0, -moveVertical);
-				lookDirection = Quaternion.AngleAxis (cameraTargetDirection, Vector3.up) * lookDirection;
-                float step = turnSpeed * Time.deltaTime;
-                var lookRot = Quaternion.LookRotation(lookDirection.normalized);
-                character.transform.rotation = Quaternion.RotateTowards(character.transform.rotation, lookRot, step);
+            if (Input.GetButtonDown("Fire2")) {
+                //Camera.main.transform.RotateAround(transform.position, Vector3.up, cameraTurnAmount);
+                //Camera.main.transform.Rotate(new Vector3(0, cameraTurnAmount, 0));
+                cameraHolder.transform.RotateAround(transform.position, Vector3.up, cameraTurnAmount);
+                cameraTargetDirection += cameraTurnAmount;
+                if (cameraTargetDirection > 359f) {
+                    cameraTargetDirection = 0f;
+                }
             }
-        }
 
-        // Cancel gravity, move to position.
-        moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
+            // Handle movement.
+            if (controller.isGrounded) {
+                moveDirection = new Vector3(moveHorizontal, 0, moveVertical);
+                moveDirection = Quaternion.AngleAxis(cameraTargetDirection, Vector3.up) * moveDirection;
+                moveDirection = transform.TransformDirection(moveDirection);
+                moveDirection *= moveSpeed;
+                if (Input.GetButton("Jump")) {
+                    moveDirection.y = jumpSpeed;
+                }
+
+                // Turn if we need to turn.
+                if (moveVertical != 0 || moveHorizontal != 0) {
+                    lookDirection = new Vector3(-moveHorizontal, 0, -moveVertical);
+                    lookDirection = Quaternion.AngleAxis(cameraTargetDirection, Vector3.up) * lookDirection;
+                    float step = turnSpeed * Time.deltaTime;
+                    var lookRot = Quaternion.LookRotation(lookDirection.normalized);
+                    character.transform.rotation = Quaternion.RotateTowards(character.transform.rotation, lookRot, step);
+                }
+            }
+
+            // Cancel gravity, move to position.
+            moveDirection.y -= gravity * Time.deltaTime;
+            controller.Move(moveDirection * Time.deltaTime);
+        } else {
+            // player not in control, lerp to position
+            m_Animator.SetBool("Walking", !inControl);
+
+        }
 
         // Report location to fog shader.
         Shader.SetGlobalVector("_Origin", transform.position);
