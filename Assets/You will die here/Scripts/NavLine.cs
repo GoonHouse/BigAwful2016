1da3ad045ea0d825e34aa762fd9210a8f7625464
EntranceJew <EntranceJew@gmail.com>
2016-07-18 10:47:54 -0400

    Preserved title screen's cinematic experience.

--- Assets/You will die here/Scripts/NavLine.cs
+++ Assets/You will die here/Scripts/NavLine.cs
@@ -5,6 +5,8 @@ public class NavLine : MonoBehaviour {
 	private GameObject gramps;
     private Vector3 grampsHeight = new Vector3(0.0f, 4.5f, 0.0f);
     public float grampsLerp = 0.91f;
+    public float mitigation = 1.0f;
+
 	// Use this for initialization
 	void Start () {
 		gramps = GameObject.Find("GrampsHolder");
@@ -14,6 +16,6 @@ public class NavLine : MonoBehaviour {
 	void Update () {
 		LineRenderer lineRenderer = GetComponent<LineRenderer>();
 		lineRenderer.SetPosition (0, transform.position+grampsHeight);
-		lineRenderer.SetPosition (1, Vector3.Lerp(transform.position+grampsHeight, gramps.transform.position-Vector3.up+grampsHeight, grampsLerp));
+		lineRenderer.SetPosition (1, Vector3.Lerp(transform.position+grampsHeight, gramps.transform.position-Vector3.up+grampsHeight, mitigation * grampsLerp));
 	}
 }
---
+++ Assets/You will die here/Scripts/TitleSceneCinematicExperiencePreserver.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TitleSceneCinematicExperiencePreserver : MonoBehaviour {
+
+    public float mitigationStart = 0.5f;
+    public float mitigationEnd = 1.0f;
+
+    public bool  isChanging = true;
+    public float timeToChange = 16.0f;
+    public float currentTimeToChange = 0.0f;
+
+    private List<NavLine> navs;
+
+	// Use this for initialization
+	void Start () {
+        navs = new List<NavLine>( GameObject.Find("Rooms").GetComponentsInChildren<NavLine>() );
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (isChanging) {
+            currentTimeToChange += Time.deltaTime;
+            foreach( NavLine nav in navs) {
+                nav.mitigation = Mathf.LerpUnclamped(mitigationStart, mitigationEnd, currentTimeToChange / timeToChange);
+            }
+            if (currentTimeToChange >= timeToChange) {
+                currentTimeToChange = 0.0f;
+                isChanging = false;
+                foreach (NavLine nav in navs) {
+                    nav.mitigation = mitigationEnd;
+                }
+            }
+        }
+	}
+}
