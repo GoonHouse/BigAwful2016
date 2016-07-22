f0a71e476d99d787d5434d247fb61043c7c17fe6
TomRintjema <tomrintjema@hotmail.com>
2016-07-22 13:06:26 -0400

    Made the Corrupt.cs touch UnderFloor.mat as well.
    Maybe we should roll this into the fog controller so we can make the fog dark red or something too.

--- Assets/You will die here/Scripts/Corrupt.cs
+++ Assets/You will die here/Scripts/Corrupt.cs
@@ -5,10 +5,12 @@ public class Corrupt : MonoBehaviour {
 	public Renderer rend;
 	void Start() {
 		rend = GetComponent<Renderer>();
-		rend.sharedMaterial.shader = Shader.Find("Custom/LinearFogTransparent");
+		rend.sharedMaterials[0].shader = Shader.Find("Custom/LinearFogTransparent");
+		rend.sharedMaterials[1].shader = Shader.Find("Custom/LinearFog");
 	}
 	void Update() {
-		float corruption = Mathf.PingPong(Time.time, 1.0F);
-		rend.sharedMaterial.SetFloat("_AltValue", corruption);
+		float corruption = Mathf.PingPong(Time.time*0.01f, 1.0F);
+		rend.sharedMaterials[0].SetFloat("_AltValue", corruption);
+		rend.sharedMaterials[1].SetColor ("_Color", Color.Lerp (new Color32 (189, 189, 189, 255), Color.black, corruption));
 	}
 }
