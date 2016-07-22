3e7358cd0aa538e9879e30c70b9462bd8bdb7e45
TomRintjema <tomrintjema@hotmail.com>
2016-07-22 12:46:43 -0400

    made a crummy script that will change the shader values for the Walls.mat

---
+++ Assets/You will die here/Scripts/Corrupt.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Corrupt : MonoBehaviour {
+	public Renderer rend;
+	void Start() {
+		rend = GetComponent<Renderer>();
+		rend.sharedMaterial.shader = Shader.Find("Custom/LinearFogTransparent");
+	}
+	void Update() {
+		float corruption = Mathf.PingPong(Time.time, 1.0F);
+		rend.sharedMaterial.SetFloat("_AltValue", corruption);
+	}
+}
