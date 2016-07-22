752f304aa1281335cc36ae49e4d9257e1af98bd2
TomRintjema <tomrintjema@hotmail.com>
2016-07-22 11:12:32 -0400

    WorldSpike.prefab

---
+++ Assets/You will die here/Scripts/WorldSpike.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldSpike : MonoBehaviour {
+	private SkinnedMeshRenderer Spike;
+	public float TimeToReshape = 1f;
+	public bool Animate = false;
+	private float timer = 0f;
+		
+
+	// Use this for initialization
+	void Start () {
+		Spike = GetComponent<SkinnedMeshRenderer> ();
+		reshape ();
+	}
+		// Update is called once per frame
+	void Update () {
+		if (Animate) {
+			timer -= Time.deltaTime;
+			if (timer < 0) {
+				timer = TimeToReshape;
+				reshape ();
+			}
+		}
+	}
+		void reshape() {
+		int targets = Spike.sharedMesh.blendShapeCount;
+		for(int i = 0; i < targets; i++)
+		{
+			float foo = Random.Range (0, 100);
+			Spike.SetBlendShapeWeight (i, foo);
+			}
+		}
+}
